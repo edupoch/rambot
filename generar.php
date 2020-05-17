ac07c2b9975d75d8ea491fb36fb4f9410082d135
Edu Poch <edupoch@gmail.com>
2020-05-17 17:36:53 +0200

    Interfaz sencilla

---
+++ generar.php
@@ -0,0 +1,225 @@
+<?php 
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
+error_reporting(E_ALL);
+
+$conceptosArtur = ['buildings'];
+$conceptosXulian = ['puke'];
+
+function muestraImagen($imagen) {
+	header('Content-Type: image/png');
+	echo $imagen;
+	die();
+}
+
+function obtenerImagen($config, $nombre, $concepto) {
+	$client = new \GuzzleHttp\Client();
+
+	$response = $client->request('GET', 'https://customsearch.googleapis.com/customsearch/v1',
+	[
+		'query' => [
+			'q' => $concepto,
+			'num' => 1,
+			'start' => rand(1, 99),
+			'imgSize' => 'xlarge',
+			'searchType' => 'image',
+			'safe' => 'off',
+			'key' => $config['GOOGLE_KEY'],			
+			'cx' => $config['GOOGLE_CX'],
+		]
+	]);
+
+	// echo $response->getStatusCode();
+	
+	$body = json_decode($response->getBody(), true);
+
+	if ($body && isset($body['items']) && count($body['items'])) {
+		$link = $body['items'][0]['link'];
+		$pathinfo = pathinfo($link);
+
+		$fichero = 'imgs/' . $nombre . '.png';
+
+		$client->request('GET', $body['items'][0]['link'], ['sink' => $fichero]);
+
+		return $fichero;
+	}
+
+	return null;
+
+}
+
+function creaCoordenadasDeMascara($tamano) {
+	$metodo = rand(1, 5);
+	$ancho = rand(10, 40);
+
+	switch ($metodo) {
+	 	case 1:
+	 		//Diagonales der-izq
+	 		$coordenadas = [];
+
+	 		$x = 0;
+	 		$y = 0;
+	 		for ($x = 0; $x < 2 * $tamano; $x = $x + 2 * $ancho) {
+	 			$coordenadas[] = [
+	 				['x' => $x, 'y' => 0],
+	 				['x' => 0, 'y' => $x],
+	 				['x' => 0, 'y' => $x + $ancho],
+	 				['x' => $x + $ancho, 'y' => 0],
+	 			];
+	 		}
+	 		break;
+
+	 	case 2: 
+	 		// Diagonales izq-der
+	 		$coordenadas = [];
+
+	 		$x = 0;
+	 		$y = 0;
+	 		for ($x = 0; $x < 2 * $tamano; $x = $x + 2 * $ancho) {
+	 			$coordenadas[] = [
+	 				['x' => 0, 'y' => $tamano - $ancho - $x],
+	 				['x' => $x + $ancho, 'y' => $tamano],
+	 				['x' => $x, 'y' => $tamano],
+	 				['x' => 0, 'y' => $tamano - $x],
+	 			];
+	 		}
+	 		break;
+
+	 	case 3: 
+	 		// Líneas verticales
+	 		$coordenadas = [];
+
+	 		$x = 0;
+	 		$y = 0;
+	 		for ($x = 0; $x < $tamano; $x = $x + 2 * $ancho) {
+	 			$coordenadas[] = [
+	 				['x' => $x, 'y' => 0],
+	 				['x' => $x + $ancho, 'y' => 0],
+	 				['x' => $x + $ancho, 'y' => $tamano],
+	 				['x' => $x, 'y' => $tamano],
+	 			
+	 			];
+	 		}
+	 		break;
+
+	 	case 4: 
+	 		// Líneas horizontales
+	 		$coordenadas = [];
+
+	 		$x = 0;
+	 		$y = 0;
+	 		for ($x = 0; $x < $tamano; $x = $x + 2 * $ancho) {
+	 			$coordenadas[] = [
+	 				['x' => 0, 'y' => $x],
+	 				['x' => $tamano, 'y' => $x],
+	 				['x' => $tamano, 'y' => $x + $ancho],
+	 				['x' => 0, 'y' => $x + $ancho],
+	 			];
+	 		}
+	 		break;
+
+	 	case 5: 
+	 		// Cuadrado en el centro
+	 		$coordenadas = [];
+	 		$anchoCuadrado = rand($tamano / 5, $tamano / 2);
+
+ 			$coordenadas[] = [
+ 				['x' => $tamano / 2 - $anchoCuadrado,'y' => $tamano / 2 - $anchoCuadrado],
+ 				['x' => $tamano / 2 + $anchoCuadrado,'y' => $tamano / 2 - $anchoCuadrado],
+ 				['x' => $tamano / 2 + $anchoCuadrado,'y' => $tamano / 2 + $anchoCuadrado],
+ 				['x' => $tamano / 2 - $anchoCuadrado,'y' => $tamano / 2 + $anchoCuadrado],
+ 			];
+ 			break;	
+	}
+
+	return $coordenadas;
+}
+
+function creaMascara($tamano, $coordenadas, $colorFondo, $colorMascara) {
+	$mask = new Imagick();
+	$mask->newimage($tamano, $tamano, new ImagickPixel($colorFondo));
+	$mask->setimageformat('png');
+
+	$polygon = new ImagickDraw();
+	$polygon->setFillColor(new ImagickPixel($colorMascara));
+	foreach ($coordenadas as $cs) {
+		$polygon->polygon($cs);
+	}
+	$mask->drawimage($polygon);
+
+	return $mask;
+}
+
+function aplicaMascara($mask, $fichero) {
+	$maskDimensions = $mask->getImageGeometry();
+
+	$image = new Imagick();
+	$image->readimage($fichero);
+	$imageDimensions = $image->getImageGeometry();
+	$maskX = rand(0, $imageDimensions['width'] - $maskDimensions['width']);
+	$maskY = rand(0, $imageDimensions['height'] - $maskDimensions['height']);
+	$image->setImageFormat('png');
+	$image->setImageVirtualPixelMethod(Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
+	$image->setImageMatte(true);
+	$image->compositeimage($mask, Imagick::COMPOSITE_DSTIN, $maskX, $maskY, Imagick::CHANNEL_ALPHA);
+	$image->cropImage($maskDimensions['width'], $maskDimensions['height'], $maskX, $maskY);
+
+	return $image;
+}
+
+$conceptoArtur = $conceptosArtur[rand(0, count($conceptosArtur) - 1)];
+$conceptoXulian = $conceptosXulian[rand(0, count($conceptosXulian) - 1)];
+
+$xulian = obtenerImagen($config, 'xulian', $conceptoXulian);
+$artur = obtenerImagen($config, 'artur', $conceptoArtur);
+
+// $artur = 'imgs/artur.png';
+// $xulian = 'imgs/xulian.png';
+
+$tamano = 500;
+
+$coordenadas = creaCoordenadasDeMascara($tamano);
+
+$mask = creaMascara($tamano, $coordenadas,'transparent', 'black');
+
+//muestraImagen($mask);
+
+// Artur
+
+$imageArtur = aplicaMascara($mask, $artur);
+
+//muestraImagen($imageArtur);
+
+// Xulian
+
+$mask2 = creaMascara($tamano, $coordenadas, 'black', 'white');
+$mask2->transparentPaintImage(new ImagickPixel('white'), 0, 1, false);
+
+//muestraImagen($mask2);
+
+$imageXulian = aplicaMascara($mask2, $xulian);
+
+//muestraImagen($imageXulian);
+
+// Combinación
+
+$resultado = new Imagick();
+$resultado->newimage($tamano, $tamano, new ImagickPixel('transparent'));
+$resultado->setimageformat('png');
+
+$resultado->compositeImage($imageXulian, Imagick::COMPOSITE_BLEND, 0, 0);
+$resultado->compositeImage($imageArtur, Imagick::COMPOSITE_BLEND, 0, 0);
+
+//muestraImagen($resultado);
+
+$ficheroResultado = 'imgs/resultado.png';
+$resultado->writeImage($ficheroResultado);
+
+echo json_encode([
+	'img' => $ficheroResultado,
+	'conceptos' => '#' . $conceptoArtur . ' #' . $conceptoXulian
+]);
+
+?>
--- index.php
+++ index.php
@@ -1,257 +1,56 @@
-<?php 
-
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config.php';
-
-error_reporting(E_ALL);
-
-$conceptosArtur = ['buildings'];
-$conceptosXulian = ['puke'];
-
-function muestraImagen($imagen) {
-	header('Content-Type: image/png');
-	echo $imagen;
-	die();
-}
-
-function obtenerImagen($config, $nombre, $conceptos) {
-
-	// $fb = new \Facebook\Facebook([
-	//   'app_id' => '278068986920244',
-	//   'app_secret' => '1e8e2e04adab9fa05d863770559ec023',
-	//   //'default_graph_version' => 'v2.10',
-	//   //'default_access_token' => '{access-token}', // optional
-	// ]);
-
-	// try {
-	//   // Get the \Facebook\GraphNode\GraphUser object for the current user.
-	//   // If you provided a 'default_access_token', the '{access-token}' is optional.
-	//   $response = $fb->get('/me', '{access-token}');
-	// } catch(\Facebook\Exception\FacebookResponseException $e) {
-	//   // When Graph returns an error
-	//   echo 'Graph returned an error: ' . $e->getMessage();
-	//   exit;
-	// } catch(\Facebook\Exception\FacebookSDKException $e) {
-	//   // When validation fails or other local issues
-	//   echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	//   exit;
-	// }
-
-	// // $me = $response->getGraphUser();
-	// // echo 'Logged in as ' . $me->getName();
-
-	// $helper = $fb->getRedirectLoginHelper();
-
-	// $permissions = ['instagram_basic'];
-	// $loginUrl = $helper->getLoginUrl('https://ram.ocre.soy', $permissions);
-
-	// echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-
-	// Create a client with a base URI
-	// $client = new \GuzzleHttp\Client(['base_uri' => 'https://api.github.com/repos/']);
-	// $response = $client->request('GET', 'guzzle/guzzle');
-
-	$client = new \GuzzleHttp\Client();
-
-
-	$response = $client->request('GET', 'https://customsearch.googleapis.com/customsearch/v1',
-	[
-		'query' => [
-			'q' => $conceptos[rand(0, count($conceptos) - 1)],
-			'num' => 1,
-			'start' => rand(1, 99),
-			//'start' => 1,
-			'imgSize' => 'xlarge',
-			//'imgType' => 'photo',
-			'searchType' => 'image',
-			'key' => $config['GOOGLE_KEY'],
-			'safe' => 'off',
-			'cx' => $config['GOOGLE_CX'],
-			//'filetype' => 'png'
-			//'dateRestrict' => 'm1',
-
-		]
-	]);
-
-	// echo $response->getStatusCode();
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Rambot</title>
+	<link rel="stylesheet" href="normlize.css">
 	
-	$body = json_decode($response->getBody(), true);
+	<style>
+		.wrapper {
+			max-width: 500px;
+			margin: 0 auto;
+		}
 
-	if ($body && isset($body['items']) && count($body['items'])) {
-		$link = $body['items'][0]['link'];
-		$pathinfo = pathinfo($link);
+		img, textarea {
+			display: block;
+			width: 100%;
+			margin-bottom: 20px;
+		}
 
-		//$fichero = $nombre . '.' . $pathinfo['extension'];
-		$fichero = 'imgs/' . $nombre . '.png';
+		button {
+			margin: 0 auto;
+		}
 
-		$client->request('GET', $body['items'][0]['link'], ['sink' => $fichero]);
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="js-estado"></div>
+	</div>
 
-		return $fichero;
-	}
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script>
+		function generarImagen() {
+			$estado = $('.js-estado');
 
-	return null;
+			$estado.html('Generando imagen...');
 
-}
+			$.get('generar.php', '', function(resultado) {
+				console.log(resultado);
+				if (resultado && resultado.img) {
+					html = '';
+					html += '<img src="' + resultado.img + '?v=' + Date.now() + '" />';
+					html += '<textarea>' + resultado.conceptos + '</textarea>';
+					html += '<button onclick="generarImagen()">Generar otra imagen</button>'
+					$estado.html(html);
+				} else {
+					$estado.html('No se ha podido generar la imagen');
+				}
+			}, 'json');
+		}
 
-function creaCoordenadasDeMascara($tamano) {
-	$metodo = rand(1, 5);
-	$ancho = rand(10, 40);
-
-	switch ($metodo) {
-	 	case 1:
-	 		//Diagonales der-izq
-	 		$coordenadas = [];
-
-	 		$x = 0;
-	 		$y = 0;
-	 		for ($x = 0; $x < 2 * $tamano; $x = $x + 2 * $ancho) {
-	 			$coordenadas[] = [
-	 				['x' => $x, 'y' => 0],
-	 				['x' => 0, 'y' => $x],
-	 				['x' => 0, 'y' => $x + $ancho],
-	 				['x' => $x + $ancho, 'y' => 0],
-	 			];
-	 		}
-	 		break;
-
-	 	case 2: 
-	 		// Diagonales izq-der
-	 		$coordenadas = [];
-
-	 		$x = 0;
-	 		$y = 0;
-	 		for ($x = 0; $x < 2 * $tamano; $x = $x + 2 * $ancho) {
-	 			$coordenadas[] = [
-	 				['x' => 0, 'y' => $tamano - $ancho - $x],
-	 				['x' => $x + $ancho, 'y' => $tamano],
-	 				['x' => $x, 'y' => $tamano],
-	 				['x' => 0, 'y' => $tamano - $x],
-	 			];
-	 		}
-	 		break;
-
-	 	case 3: 
-	 		// Líneas verticales
-	 		$coordenadas = [];
-
-	 		$x = 0;
-	 		$y = 0;
-	 		for ($x = 0; $x < $tamano; $x = $x + 2 * $ancho) {
-	 			$coordenadas[] = [
-	 				['x' => $x, 'y' => 0],
-	 				['x' => $x + $ancho, 'y' => 0],
-	 				['x' => $x + $ancho, 'y' => $tamano],
-	 				['x' => $x, 'y' => $tamano],
-	 			
-	 			];
-	 		}
-	 		break;
-
-	 	case 4: 
-	 		// Líneas horizontales
-	 		$coordenadas = [];
-
-	 		$x = 0;
-	 		$y = 0;
-	 		for ($x = 0; $x < $tamano; $x = $x + 2 * $ancho) {
-	 			$coordenadas[] = [
-	 				['x' => 0, 'y' => $x],
-	 				['x' => $tamano, 'y' => $x],
-	 				['x' => $tamano, 'y' => $x + $ancho],
-	 				['x' => 0, 'y' => $x + $ancho],
-	 			];
-	 		}
-	 		break;
-
-	 	case 5: 
-	 		// Cuadrado en el centro
-	 		$coordenadas = [];
-	 		$anchoCuadrado = rand($tamano / 5, $tamano / 2);
-
- 			$coordenadas[] = [
- 				['x' => $tamano / 2 - $anchoCuadrado,'y' => $tamano / 2 - $anchoCuadrado],
- 				['x' => $tamano / 2 + $anchoCuadrado,'y' => $tamano / 2 - $anchoCuadrado],
- 				['x' => $tamano / 2 + $anchoCuadrado,'y' => $tamano / 2 + $anchoCuadrado],
- 				['x' => $tamano / 2 - $anchoCuadrado,'y' => $tamano / 2 + $anchoCuadrado],
- 			];
- 			break;	
-	}
-
-	return $coordenadas;
-}
-
-function creaMascara($tamano, $coordenadas, $colorFondo, $colorMascara) {
-	$mask = new Imagick();
-	$mask->newimage($tamano, $tamano, new ImagickPixel($colorFondo));
-	$mask->setimageformat('png');
-
-	$polygon = new ImagickDraw();
-	$polygon->setFillColor(new ImagickPixel($colorMascara));
-	foreach ($coordenadas as $cs) {
-		$polygon->polygon($cs);
-	}
-	$mask->drawimage($polygon);
-
-	return $mask;
-}
-
-function aplicaMascara($mask, $fichero) {
-	$maskDimensions = $mask->getImageGeometry();
-
-	$image = new Imagick();
-	$image->readimage($fichero);
-	$imageDimensions = $image->getImageGeometry();
-	$maskX = rand(0, $imageDimensions['width'] - $maskDimensions['width']);
-	$maskY = rand(0, $imageDimensions['height'] - $maskDimensions['height']);
-	$image->setImageFormat('png');
-	$image->setImageVirtualPixelMethod(Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
-	$image->setImageMatte(true);
-	$image->compositeimage($mask, Imagick::COMPOSITE_DSTIN, $maskX, $maskY, Imagick::CHANNEL_ALPHA);
-	$image->cropImage($maskDimensions['width'], $maskDimensions['height'], $maskX, $maskY);
-
-	return $image;
-}
-
-$xulian = obtenerImagen($config, 'xulian', $conceptosXulian);
-$artur = obtenerImagen($config, 'artur', $conceptosArtur);
-
-// $artur = 'imgs/artur.png';
-// $xulian = 'imgs/xulian.png';
-
-$tamano = 500;
-
-$coordenadas = creaCoordenadasDeMascara($tamano);
-
-$mask = creaMascara($tamano, $coordenadas,'transparent', 'black');
-
-//muestraImagen($mask);
-
-// Artur
-
-$imageArtur = aplicaMascara($mask, $artur);
-
-//muestraImagen($imageArtur);
-
-// Xulian
-
-$mask2 = creaMascara($tamano, $coordenadas, 'black', 'white');
-$mask2->transparentPaintImage(new ImagickPixel('white'), 0, 1, false);
-
-//muestraImagen($mask2);
-
-$imageXulian = aplicaMascara($mask2, $xulian);
-
-//muestraImagen($imageXulian);
-
-// Combinación
-
-$resultado = new Imagick();
-$resultado->newimage($tamano, $tamano, new ImagickPixel('transparent'));
-$resultado->setimageformat('png');
-
-$resultado->compositeImage($imageXulian, Imagick::COMPOSITE_BLEND, 0, 0);
-$resultado->compositeImage($imageArtur, Imagick::COMPOSITE_BLEND, 0, 0);
-
-muestraImagen($resultado);
-
-?>
+		$(function() {
+			generarImagen();
+		});
+	</script>	
+</body>
+</html>
